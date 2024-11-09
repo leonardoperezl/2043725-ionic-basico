66b96d9e5351b37c7c30e0fbec19d3001f1e3b71
Leonardo Pérez Lázaro <leonardo.perezlazaro@gmail.com>
2024-11-09 09:42:18 -0600

    Practica 11: Formularios

--- miApp-2043725/src/app/app.component.ts
+++ miApp-2043725/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
 import { RouterOutlet } from '@angular/router';
+import { IonApp } from "@ionic/angular/standalone";
+import { ListaAlumnosComponent } from "./lista-alumnos/lista-alumnos.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [IonApp, RouterOutlet, ListaAlumnosComponent],
 })
 export class AppComponent {
   constructor() {}
--- miApp-2043725/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-2043725/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,44 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonList, IonItem, IonLabel, IonItemSliding, IonItemOptions, IonItemOption, IonActionSheet, IonButton, ActionSheetController } from "@ionic/angular/standalone";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonInput, IonList, IonCheckbox, IonItem, IonLabel, IonItemSliding, IonItemOptions, IonItemOption, IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonIcon } from "@ionic/angular/standalone";
+
+import { Alumno } from '../alumno.model';
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
   styleUrls: ['./lista-alumnos.component.scss'],
   standalone: true,
-  imports: [IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonItem, IonList, CommonModule],
+  imports: [IonIcon, IonCol, IonRow, IonGrid, IonContent, IonTitle, IonToolbar, IonHeader, FormsModule, ReactiveFormsModule, IonItemOption, IonItemOptions, IonItemSliding, IonLabel, IonItem, IonList, CommonModule, IonButton, IonCheckbox, IonInput]
 })
 export class ListaAlumnosComponent {
-  alumnos: string[];
+  alumnos: Alumno[] = [];
+  nuevoAlumno: Alumno = { nombre: "", presente: false }
 
-  constructor(private actionSheetCtrl: ActionSheetController) { 
-    this.alumnos = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"]
+  constructor() {}
+
+  agregarAlumno(): void {
+    this.alumnos.push({ ...this.nuevoAlumno  });
+    this.nuevoAlumno = {
+      nombre: "",
+      presente: false
+    };
   }
 
-  async presentActionSheet() {
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: "Opciones",
-      buttons: [
-        {
-          text: 'Eliminar',
-          role: 'destructive',
-          data: {
-            action: 'delete',
-          },
-        },
-        {
-          text: 'Cancelar',
-          role: 'cancel',
-          data: {
-            action: 'cancel',
-          },
-        },
-      ]
-    })
-
-    await actionSheet.present();
-  }
-
-  // ngOnInit() {}
+  ngOnInit() {}
 }
